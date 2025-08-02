655ed5b9556d1a01f152ca7894d9efa7089b4ba1
daze-glitch <timonvenom@gmail.com>
2025-08-02 16:52:15 +0300

    v1.11

--- script.lua
+++ script.lua
@@ -2,40 +2,69 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "✦ rizx TSB panel ✦",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 0,
    LoadingTitle = "Loading rizx TSB panel",
    LoadingSubtitle = "by rizx",
-   ShowText = "rizx panel", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
+   ShowText = "rizx panel",
+   Theme = "Ocean",
+   ToggleUIKeybind = "K",
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
+   DisableBuildWarnings = false,
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "rizx hub"
    },
-
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
-
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "✦ rizx TSB panel ✦ | Key ",
       Subtitle = "Key System",
-      Note = "get key from the link ", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/MJA6t1DB"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "get key from our discord server.",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = true,
+      Key = {"https://pastebin.com/raw/MJA6t1DB"}
    }
 })
 
-local MainTab = Window:CreateTab("Home", 170940874) -- Title, Image
-local Section = Tab:CreateSection("Halooo")
+local MainTab = Window:CreateTab("Home", 170940874)
+local Section = MainTab:CreateSection("Halooo")
+
+local Players = game:GetService("Players")
+local spawnBlock = nil
+
+local Button = MainTab:CreateButton({
+    Name = "Button Example",
+    Callback = function()
+        for _, player in pairs(Players:GetPlayers()) do
+            hookCharacterRespawn(player)
+        end
+
+        Players.PlayerAdded:Connect(function(player)
+            hookCharacterRespawn(player)
+        end)
+    end,
+})
+
+function hookCharacterRespawn(player)
+    player.CharacterAdded:Connect(function(character)
+        local hrp = character:WaitForChild("HumanoidRootPart")
+
+        spawnBlock = Instance.new("Part")
+        spawnBlock.Size = Vector3.new(6, 1, 6)
+        spawnBlock.Position = hrp.Position -- блок в позиции игрока
+        spawnBlock.Anchored = true
+        spawnBlock.CanCollide = false
+        spawnBlock.Transparency = 0.5
+        spawnBlock.Name = "RespawnBlock"
+        spawnBlock.Parent = workspace
+
+        -- Переместим игрока немного выше, чтобы он точно оказался в блоке
+        hrp.CFrame = spawnBlock.CFrame + Vector3.new(0, 2, 0)
+    end)
+end
